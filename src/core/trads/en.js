93047990f4e4a515fa4d621581cff62552080754
SMontenez <sebastien.montenez@gmail.com>
2019-03-04 14:48:54 +0100

    Add some english trads

--- src/core/trads/en.js
+++ src/core/trads/en.js
@@ -1,10 +1,107 @@
 export default {
+  common: {
+    sendButton: 'Send',
+    requiredInputLabel: '* Mandatory',
+    firstname: 'Firstname',
+    name: 'Last name',
+    email: 'Email',
+    message: 'Your message',
+  },
   Adresses: {
-    title: 'Addresses',
-    description: 'You will find here all the addresses you\'ll need to be with us during August, the 17th',
-    openOSM: 'Enlarge map'
+    title: 'Address',
+    description: 'You will find hereunder all the addresses you need to follow us on the 17th.',
+    church: 'Church Notre-Dame-des-Neiges',
+    castle: 'Château de la Taillée',
+    openOSM: 'Zoom in',
+  },
+  Bienvenue: {
+    title: 'Welcome!',
+    text1:
+      'After weeks of hesitation, hours of reflexion and some months of relation, we decided it was time to get married!',
+    text2:
+      'You should have received the invtation now but if you need more information, if you lost it or if you are digital native... this website is made for you!',
+    text3: "Can't wait to see you on the 17th ;-)",
+  },
+  Contact: {
+    title: 'Contact',
+    description:
+      "Despite our efforts, if you still can't find what you're looking for, feel free to send us a message and we will reply shortly.",
+    error: "Oups, looks like your message couldn't be send. Please try again later.",
+    success: "Yeah! Got your message!",
+  },
+  Covoiturage: {
+    title: 'Carpooling',
+    description:
+      'You have a spare seat? Or you are looking for a pilote? As Chauffeur Privé is not (yet) in Deux Sevres we suggest you can do some carpooling.',
+    proposalsLabel: 'Offered travels',
+    requestsLabel: 'Requested travels',
+    addProposal: 'Offer a travel',
+    addRequest: 'Add your request',
+    form: {
+      date: 'Date',
+      arrival: 'Arrivals',
+      nbPlaces: 'Seats available',
+      email: 'Email',
+      phone: 'Phone',
+      oneFieldMandatory: '(One field mandatory at least)',
+    },
+    error: {
+      proposal:
+        "Sorry, something wrong happened and your travel couldn't be registered. Please try again later.",
+      request:
+        "Sorry, something wrong happened and your request couldn't be registered. Please try again later.",
+    },
+    success: {
+      proposal: 'Your travel is now created!',
+      request: 'Your request is now created!',
+    },
+  },
+  DressCode: {
+    title: 'Dresscode',
+    text1:
+      'You may have understood it with our invitation and the website, our wedding will be "bucolic and chic". If you want to blend into the background, some tips for your outfit.',
+    text2:
+      "Couleurs pastels et motifs à fleurs seront à l'honneur. N'hésitez pas à sortir vos robes en dentelles et vos noeuds papillon.",
+    text3: 'Need some inspiration?',
   },
   EnConstruction: {
-    title: 'Coming soon...'
+    title: 'Coming soon...',
   },
-}
+  Liste: {
+    title: 'Wedding list',
+    text1:
+      'Even if you being there is the most important gift for us, we know some of you would like to offer us something more material.',
+    text2BeforeLink: 'You will find ',
+    text2Link: 'here',
+    text2AfterLink: ' our wedding list.',
+  },
+  Logements: {
+    title: 'Logements',
+    text1: "Fan de camping ? Adepte des hôtels ? Envie d'une chambre chez l'habitant ?",
+    text2: 'Nous avons compilé toutes les options pour vous.',
+  },
+  Menu: {
+    adresses: 'adresses',
+    bienvenue: 'bienvenue',
+    contact: 'contact',
+    covoiturage: 'covoiturage',
+    logements: 'logements',
+    presence: 'presence',
+    jeux: 'jeux',
+    listeDeMariage: 'liste de mariage',
+    dressCode: 'dress code',
+  },
+  Presence: {
+    title: 'Présence',
+    description: 'Pas envie d’envoyer la carte ? Confirmez votre présence ici !',
+    isPresent: 'Je serai présent',
+    isAbsent: 'Regrette de ne pouvoir venir',
+    nbAdults: "Nombre d'adultes",
+    nbChildren: "Nombre d'enfants",
+    detailsTitle:
+      'Age des enfants, allergies alimentaires, besoins spécifiques ? Dites-nous tout !',
+    error:
+      'Désolé, une erreur est survenue et votre présence n&#39;a pas pu être enregistrée. Veuillez réessayer plus tard.',
+    success: 'Votre présence est enregistrée, merci !',
+  },
+};
